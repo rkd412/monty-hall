661e7c4ab617d2ca527c03ef067c92c0f2701366
Robert Davis <rkdavis15@gmail.com>
2021-07-27 14:05:30 -0400

    Displayed doors are shuffled now

--- src/components/Door.js
+++ src/components/Door.js
@@ -1,11 +1,10 @@
 import styles from "./Door.module.css";
-import goat from '../assets/goat.jpg'
-import car from '../assets/car.jpg'
-const DoorCard = () => {
+
+const DoorCard = (props) => {
   return (
     <div className={styles["door"]}>
-      <h1 className={styles["door-text"]}>Something</h1>
-      <img className={styles["image"]} src={goat} />
+      <h1 className={styles["door-text"]}>{props.title}</h1>
+      <img className={styles["image"]} src={props.src} />
     </div>
   );
 };
--- src/components/DoorsBox.js
+++ src/components/DoorsBox.js
@@ -1,13 +1,36 @@
-import Door from './Door'
+import React, { useEffect, useState } from "react";
+
+import Door from "./Door";
 
 import styles from "./DoorsBox.module.css";
 
+import goat from "../assets/goat.jpg";
+import car from "../assets/car.jpg";
+
+const doorArr = [car, goat, goat];
+
+function shuffleArray(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
 const DoorsBox = () => {
+  const [prizes, setPrizes] = useState(doorArr);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setPrizes(shuffleArray(prizes));
+    setIsLoaded(true);
+  }, []);
+
   return (
     <div className={styles["doors-box"]}>
-      <Door />
-      <Door />
-      <Door />
+      {isLoaded ? <Door src={prizes[0]} /> : null}
+      {isLoaded ? <Door src={prizes[1]} /> : null}
+      {isLoaded ? <Door src={prizes[2]} /> : null}
     </div>
   );
 };
