b276081a53fcc416da08eae7bbbb58e430083b01
Robert Davis <rkdavis15@gmail.com>
2021-11-08 19:11:31 -0500

    Hrmmm...

--- src/components/MontyHall.js
+++ src/components/MontyHall.js
@@ -154,25 +154,26 @@ it is opened. If it has the car, the other door (containing a goat)
         >
           <div className={styles["text-box"]}>
             <div className={styles["text"]}>
-            <p>
-              The Monty Hall problem initially reached fame (or infamy) when
-              Marilyn vos Savant responded to a question from reader Craig F.
-              Whitaker in her column “Ask Marilyn” for Parade magazine. Whitaker
-              posited, “Suppose you're on a game show, and you're given the
-              choice of three doors: Behind one door is a car; behind the
-              others, goats. You pick a door, say No. 1, and the host, who knows
-              what's behind the doors, opens another door, say No. 3, which has
-              a goat. He then says to you, "Do you want to pick door No. 2?" Is
-              it to your advantage to switch your choice?” Savant correctly
-              responded that of course you should switch! Contestants who switched had
-              a 2/3 chance of winning the car while those who stayed a mere 1/3
-              chance. The backlash to her conclusion was immediate from around
-              10,000 readers including about about 1,000 PhDs sending her mail
-              decisively claiming she as wrong and decrying the state of the US
-              education system. Even when presented with mathematical proofs and
-              computer simulations, many continued to refuse to be swayed. This
-              is another such simulation and I hope you enjoy it!
-            </p>
+              <p>
+                The Monty Hall problem initially reached fame (or infamy) when
+                Marilyn vos Savant responded to a question from reader Craig F.
+                Whitaker in her column “Ask Marilyn” for Parade magazine.
+                Whitaker posited, “Suppose you're on a game show, and you're
+                given the choice of three doors: Behind one door is a car;
+                behind the others, goats. You pick a door, say No. 1, and the
+                host, who knows what's behind the doors, opens another door, say
+                No. 3, which has a goat. He then says to you, "Do you want to
+                pick door No. 2?" Is it to your advantage to switch your
+                choice?” Savant correctly responded that of course you should
+                switch! Contestants who switched had a 2/3 chance of winning the
+                car while those who stayed a mere 1/3 chance. The backlash to
+                her conclusion was immediate from around 10,000 readers
+                including about about 1,000 PhDs sending her mail decisively
+                claiming she as wrong and decrying the state of the US education
+                system. Even when presented with mathematical proofs and
+                computer simulations, many continued to refuse to be swayed.
+                This is another such simulation and I hope you enjoy it!
+              </p>
             </div>
             <div className={styles["text-box-close"]}>
               <FaRegWindowClose />
@@ -298,5 +299,7 @@ it is opened. If it has the car, the other door (containing a goat)
     </div>
   );
 };
+
+/*Thanks for stoping by!*/
 
 export default MontyHall;
